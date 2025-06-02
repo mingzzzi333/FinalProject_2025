fc2e4e8404fe17fbc3bb47485a59e908862ae5f7
mingzzzi333 <importhings2@gmail.com>
2025-06-02 14:07:21 +0900

    main controller

--- FinalProject/src/main/java/finalProject/controller/IndexController.java
+++ FinalProject/src/main/java/finalProject/controller/IndexController.java
@@ -16,4 +16,41 @@ public class IndexController {
     public String loginPage() {
         return "login/login"; 
     }
+	
+    // 일반 회원 로그인 성공 시 이동
+    @GetMapping("/index")
+    public String userMain() {
+        return "index"; // => /WEB-INF/views/index.jsp
+    }
+
+    // 관리자 로그인 성공 시 이동
+    @GetMapping("/admin")
+    public String adminMain() {
+        return "adminMain"; // => /WEB-INF/views/adminMain.jsp
+    }
+	
+    //홈으로 이동
+    @GetMapping("/home")
+    public String homePage() {
+    	return "index";
+    }
+    
+	//토론장으로 이동
+	@GetMapping("/community")
+	public String communityPage() {
+	    return "community/communityAll";
+	}
+	
+	//뉴스로 이동
+	@GetMapping("/news")
+	public String newsPage() {
+		 return "news/newsMain";
+	}
+	
+	//주식정보로 이동
+	@GetMapping("/stock")
+	public String stockPage() {
+		return "stock/stockMain";
+	}	
+		
 }
